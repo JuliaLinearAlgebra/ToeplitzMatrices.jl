8318848764bb1b9fb9023376be8f23b08ad1d5a7
David Widmann <dev+github@devmotion.de>
2021-04-15 18:31:51 +0200

    Use test/Project.toml environment

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,12 @@
+using Pkg
+
+# Activate test environment on older Julia versions
+@static if VERSION < v"1.2"
+    Pkg.activate(@__DIR__)
+    Pkg.develop(PackageSpec(; path=dirname(@__DIR__)))
+    Pkg.instantiate()
+end
+
 using ToeplitzMatrices, StatsBase, Test, LinearAlgebra
 
 using Base: copyto!
