5da10122f1186463e376200acbe5eb7231d2271d
Tianyi Pu <912396513@qq.com>
2023-03-27 18:19:16 +0100

    Update hankel.jl

--- src/hankel.jl
+++ src/hankel.jl
@@ -72,9 +72,9 @@ Base.@propagate_inbounds function getindex(A::Hankel, i::Integer, j::Integer)
     @boundscheck checkbounds(A, i, j)
     return A.v[i+j-1]
 end
-similar(A::Hankel, T::Type, dims::DimsInteger{2}) = Hankel{T}(similar(A.v, T, dims[1]+dims[2]-true), dims)
-similar(A::Hankel, T::Type, dims::Tuple{Int64,Int64}) = Hankel{T}(similar(A.v, T, dims[1]+dims[2]-true), dims) # for ambiguity with `similar(a::AbstractArray, ::Type{T}, dims::Tuple{Vararg{Int64, N}}) where {T, N}` in Base
-for fun in (:zero, :copy, :similar)
+similar(A::Hankel, T::Type = eltype(A), dims::DimsInteger{2} = size(A)) = Hankel{T}(similar(A.v, T, dims[1]+dims[2]-true), dims)
+similar(A::Hankel, T::Type = eltype(A), dims::Tuple{Int64,Int64} = size(A)) = Hankel{T}(similar(A.v, T, dims[1]+dims[2]-true), dims) # for ambiguity with `similar(a::AbstractArray, ::Type{T}, dims::Tuple{Vararg{Int64, N}}) where {T, N}` in Base
+for fun in (:zero, :copy)
     @eval $fun(A::Hankel) = Hankel($fun(A.v), size(A))
 end
 for op in (:+, :-)
